9243f5b7cec93e0f28ff1d1cd9b4fdbd9e64ba69
= <=>
2018-12-27 06:04:49 +0530

    Added Application files

---
+++ Application/finished_app.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask import render_template, request
+
+# modeling packages
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.externals import joblib
+import os
+
+# create the flask object
+app = Flask(__name__)
+L1_logistic = joblib.load('model.pkl')
+
+@app.route('/')
+def home():
+    return 'Hello World!'
+
+@app.route('/titanic', methods=['GET','POST'])
+def titanic():
+    data = {}   # data object to be passed back to the web page
+    if request.form:
+        # get the input data
+        form_data = request.form
+        data['form'] = form_data
+        predict_class = float(form_data['predict_class'])
+        predict_age = float(form_data['predict_age'])
+        predict_sibsp = float(form_data['predict_sibsp'])
+        predict_parch = float(form_data['predict_parch'])
+        predict_fare = float(form_data['predict_fare'])
+        predict_sex = form_data['predict_sex']
+
+        # convert the sex from text to binary
+        if predict_sex == 'M':
+            sex = 0
+        else:
+            sex = 1
+        input_data = np.array([predict_class, predict_age, predict_sibsp, predict_parch, predict_fare, sex])
+
+        # get prediction
+        prediction = L1_logistic.predict_proba(input_data.reshape(1, -1))
+        prediction = prediction[0][1] # probability of survival
+        data['prediction'] = '{:.1f}% Chance of Survival'.format(prediction * 100)
+        
+    return render_template('titanic_finished.html', data=data)
+
+
+if __name__ == '__main__':
+    app.run(port = 5000,debug=True)
